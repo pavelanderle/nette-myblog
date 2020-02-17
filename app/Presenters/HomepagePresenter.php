73ced4fe2207f4911c86692d8d8610abc7bac8f7
Pavel Anderle <anderle@spseplzen.cz>
2020-02-17 22:15:59 +0100

    Initial commite

---
+++ app/Presenters/HomepagePresenter.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+
+
+final class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+  private $database;
+
+  public function __construct(Nette\Database\Context $database){
+    $this -> database = $database;
+  }
+
+  public function renderDefault(): void
+{
+	$this->template->posts = $this->database->table('posts')
+		->order('created_at DESC')
+		->limit(5);
+}
+}
